3450b1e2a24eaf829ad871c3fe6ed45f3679baf6
DennisFG <84360092+DennisFG@users.noreply.github.com>
2021-08-06 20:23:24 -0300

    implementado o interectLists() e o searchMinValue()

--- Program.cs
+++ Program.cs
@@ -14,79 +14,44 @@ namespace Projeto_CalculadoraC_
             Division = 4,
             Mod = 5
         }
-
-// 1 + 2 * 20 / 10 % 4
-
+        
         static void Main(string[] args)
         {
-            List<double> valuesList = new List<double>();
-            List<Operations> operationsList = new List<Operations>();
+            // List<double> valuesList = new List<double>();
+            // List<Operations> operationsList = new List<Operations>();
 
-            valuesList.Add(10);
-            valuesList.Add(6);
-            valuesList.Add(2);
-            operationsList.Add((Operations)int.Parse("1"));
-            operationsList.Add((Operations)int.Parse("3"));
+            // valuesList.Add(10);
+            // valuesList.Add(10);
+            // valuesList.Add(7);
+            // valuesList.Add(2);
 
-            Console.WriteLine(valuesList.ElementAt(0));
-            Console.WriteLine(operationsList.ElementAt(0));
+            // operationsList.Add((Operations)int.Parse("2"));
+            // operationsList.Add((Operations)int.Parse("5"));
+            // operationsList.Add((Operations)int.Parse("3"));
 
-            double res = 0;
+            // // 10 - 10 % 7 * 2 
+            // // 10 - 3 * 2
+            // // 10 - 6
+            // // 4
 
-            if (operationsList.ElementAt(0) == Operations.Sum)
-            {
-                res = valuesList.ElementAt(0) + valuesList.ElementAt(1);
-                Console.WriteLine(res);
-            }
-            if (operationsList.ElementAt(0) == Operations.Subtract)
-            {
-                res = valuesList.ElementAt(0) - valuesList.ElementAt(1);
-                Console.WriteLine(res);
+            // Console.WriteLine(interectLists(valuesList, operationsList));
 
-            }
-            if (operationsList.ElementAt(0) == Operations.Multiply)
-            {
-                res = valuesList.ElementAt(0) * valuesList.ElementAt(1);
-                Console.WriteLine(res);
-
-            }
-            if (operationsList.ElementAt(0) == Operations.Division)
-            {
-                res = valuesList.ElementAt(0) / valuesList.ElementAt(1);
-                Console.WriteLine(res);
-
-            }
-            if (operationsList.ElementAt(0) == Operations.Mod)
-            {
-                res = valuesList.ElementAt(0) % valuesList.ElementAt(1);
-                Console.WriteLine(res);
-
-            }
-
-            Console.WriteLine(res);
-
-            // foreach (var i in valuesList)
+            // foreach (var item in valuesList)
             // {
-            //     Console.WriteLine(i);
+            //     Console.WriteLine(item);
             // }
 
-            //   foreach (var i in operationsList)
-            // {
-            //     Console.WriteLine(i);
-            // }
-
-
-            // instructions();// 1. apresentar instruções de uso
-            // menu1();// 2. apresentar opções
+            instructions();// 1. apresentar instruções de uso
+            menu1();// 2. apresentar opções
         }
 
         static void instructions()
         {
-            Console.WriteLine("Instruções");
+            Console.WriteLine("Instruções de uso...");
         }
+
         static void menu1()
         {
-
             Console.WriteLine("-----");
             Console.WriteLine("Escolha uma das opções abaixo:");
             Console.WriteLine("1 - Realizar calculo");
@@ -100,8 +65,6 @@ namespace Projeto_CalculadoraC_
                 opcao = Convert.ToInt32(Console.ReadLine());
 
                 if (opcao != 1 && opcao != 2) Console.WriteLine("Opção Invalida, tente novamente!");
-                // vamos transformar em um if ternário (????)
-
             } while (opcao != 1 && opcao != 2);
 
             switch (opcao)
@@ -116,31 +79,16 @@ namespace Projeto_CalculadoraC_
 
         static void doCalc()
         {
-
             Console.Write("Digite o número de operações: ");
             int qtdOp = Int32.Parse(Console.ReadLine());
             int qtdValores = qtdOp + 1;
 
             Console.WriteLine("Digite os valores a serem operados: ");
             List<double> valuesList = new List<double>();
-            // int count = 0;
-            // do
-            // {
-            //     double input = Convert.ToDouble(Console.ReadLine());
-            //     valuesList.Add(input);
-            //     count++;
-            // } while (count < qtdValores);
             for (int i = 0; i < qtdValores; i++)
             {
                 double input = Convert.ToDouble(Console.ReadLine());
                 valuesList.Add(input);
-            }
-
-
-            foreach (double item in valuesList)
-            {
-                Console.WriteLine($"{item}, ");
-
             }
 
             Console.WriteLine("Digite as operações que serão realizadas: ");
@@ -151,87 +99,130 @@ namespace Projeto_CalculadoraC_
             {
                 Console.WriteLine($"{i + 1}ª Operação: ");
                 Operations input = (Operations)int.Parse(Console.ReadLine());
-
-                // double input = Convert.ToDouble(Console.ReadLine());
-                // valuesList.Add(input);
             }
 
-            interectLists(valuesList, operationsList);
+            double res = interectLists(valuesList, operationsList); //define um double resultado
 
-            //console
-            // + somar
-            // - subtrair
-            // * multiplicar
-            // / dividir
-            // % mod
+            Console.WriteLine($"O resultado da operação é {res}"); // imprime o resultado
 
-            // for (int i = 0; i < qtdValores - 1; i++)
-            // {
-            //     string input = Convert.Console.ReadLine();
-            //     operationsList.Add(input);
-            // }
+            // List<Double> resultsList = new List<Double>();
+            // resultsList.Add(res); // guarda o resultado em uma lista
+
+            menu2();
         }
 
         static double interectLists(List<double> vL, List<Operations> oL)
         {
             List<double> newValuesList = new List<double>();
+            foreach (var item in vL)
+            {
+                newValuesList.Add(item);
+            }
+            // newValuesList = vL;
             List<Operations> newOperationsList = new List<Operations>();
+            foreach (var item in oL)
+            {
+                newOperationsList.Add(item);
+            }
+            // newOperationsList = oL;
+            double res = 0;
 
-            // interação entre as listas
-            // buscar posição do operador com prioridade -> p
-            // interagir o elementat(p) e o  elementat(p+1) da lista de valores com o elementat(p+1) da lista de operadores
+            int indexPriority = searchMinValue(newOperationsList);
 
-
-            if (oL.ElementAt(0) == Operations.Sum)
+            if (newOperationsList.ElementAt(indexPriority) == Operations.Sum)
             {
-                double res = vL.ElementAt(0) + vL.ElementAt(1);
+                res = newValuesList.ElementAt(indexPriority) + newValuesList.ElementAt(indexPriority + 1);
             }
-            if (oL.ElementAt(0) == Operations.Subtract)
+            if (newOperationsList.ElementAt(indexPriority) == Operations.Subtract)
             {
-                double res = vL.ElementAt(0) - vL.ElementAt(1);
+                res = newValuesList.ElementAt(indexPriority) - newValuesList.ElementAt(indexPriority + 1);
             }
-            if (oL.ElementAt(0) == Operations.Multiply)
+            if (newOperationsList.ElementAt(indexPriority) == Operations.Multiply)
             {
-                double res = vL.ElementAt(0) * vL.ElementAt(1);
+                res = newValuesList.ElementAt(indexPriority) * newValuesList.ElementAt(indexPriority + 1);
             }
-            if (oL.ElementAt(0) == Operations.Division)
+            if (newOperationsList.ElementAt(indexPriority) == Operations.Division)
             {
-                double res = vL.ElementAt(0) / vL.ElementAt(1);
+                res = newValuesList.ElementAt(indexPriority) / newValuesList.ElementAt(indexPriority + 1);
             }
-            if (oL.ElementAt(0) == Operations.Mod)
+            if (newOperationsList.ElementAt(indexPriority) == Operations.Mod)
             {
-                double res = vL.ElementAt(0) % vL.ElementAt(1);
+                res = newValuesList.ElementAt(indexPriority) % newValuesList.ElementAt(indexPriority + 1);
             }
 
-            // double res = operador(vL.IndexOf(0), vl.IndexOf(1));
+// 10 1
 
             // retorna newValuesList (n) e newOperationsList (n-1)
+            newValuesList.RemoveAt(indexPriority);
+            newValuesList[indexPriority] = res;
+            newOperationsList.RemoveAt(indexPriority);
 
             if (newValuesList.Count == 1)
-                return Convert.ToDouble(newValuesList.IndexOf(0));
+                return Convert.ToDouble(newValuesList.ElementAt(0));
             else
                 return interectLists(newValuesList, newOperationsList);
-
         }
 
         static int searchMinValue(List<Operations> oL)
         {
-            // qual o index da operação da primeira operação com multiplicação ou divisão ou resto?
+            // qual o index da primeira operação com multiplicação ou divisão ou resto?
             int minValue = 0;
+
             if (oL.Contains(Operations.Multiply) || oL.Contains(Operations.Division) || oL.Contains(Operations.Mod))
             {
-                // condição caso não existe o operador, pois o retorno é -1
                 List<int> lista = new List<int>();
-                lista.Add(oL.IndexOf(Operations.Multiply));
-                lista.Add(oL.IndexOf(Operations.Division));
-                lista.Add(oL.IndexOf(Operations.Mod));
+                if (oL.IndexOf(Operations.Multiply) >= 0) lista.Add(oL.IndexOf(Operations.Multiply));
+                if (oL.IndexOf(Operations.Division) >= 0) lista.Add(oL.IndexOf(Operations.Division));
+                if (oL.IndexOf(Operations.Mod) >= 0) lista.Add(oL.IndexOf(Operations.Mod));
 
                 minValue = lista.Min();
             }
+            else
+            {
+                List<int> lista = new List<int>();
+                if (oL.IndexOf(Operations.Sum) >= 0) lista.Add(oL.IndexOf(Operations.Sum));
+                if (oL.IndexOf(Operations.Subtract) >= 0) lista.Add(oL.IndexOf(Operations.Subtract));
+
+                minValue = lista.Min();
+            }
+
             return minValue;
         }
+        static void menu2()
+        {
+            Console.WriteLine("-----");
+            Console.WriteLine("Escolha uma das opções abaixo:");
+            Console.WriteLine("1 - Realizar cálculo");
+            Console.WriteLine("2 - Consultar todos os resultados");
+            Console.WriteLine("3 - Sair");
 
+            int opcao;
+
+            do
+            {
+                Console.Write("Escolha uma das opções: ");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                if (opcao != 1 && opcao != 2 && opcao != 3) Console.WriteLine("Opção Invalida, tente novamente!");
+            } while (opcao != 1 && opcao != 2 && opcao != 3);
+
+            switch (opcao)
+            {
+                case 1:
+                    doCalc(); // execução do cálculo
+                    break;
+                // case 2:
+                //     foreach (var i in )
+                //     {
+                //         Console.WriteLine($"O resultado da {i + 1}ª operação foi {resultsList(0)}");
+                //     }
+                //     menu2();
+                case 3:
+                    return; // encerrar programa
+            }
+        }
     }
+}
 
 
 // a = 2 + 2 * 2 + 1

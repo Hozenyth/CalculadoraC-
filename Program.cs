eb6042a6c4171da096332b0677db5d7c074288d5
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-08-06 11:27:42 -0300

    Inicio teste de Prioridades

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace Projeto_CalculadoraC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Meu Primeiro Projeto - CALCULADORA");
+            Console.WriteLine();
+            Console.WriteLine("Escolha o número equivalente a operação abaixo: ");
+            Console.WriteLine("1) Somar");
+            Console.WriteLine("2) Subtrair");
+            Console.WriteLine("3) Multiplicar");
+            Console.WriteLine("4) Dividir");
+            Console.WriteLine("5) Sair");
+           
+           int opcao = Int32.Parse(Console.ReadLine());
+           Console.WriteLine($"A opçao escolhida é {opcao}");
+          
+
+           switch (opcao)
+            {
+                case 1:
+                    Console.WriteLine("somar");
+                    break;
+                case 2:
+                    Console.WriteLine("subtrair");
+                    break;
+                case 3:
+                    Console.WriteLine("multiplicar");
+                    break;
+                case 4:
+                    Console.WriteLine("dividir");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
